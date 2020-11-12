2db44476b72be35b52cdb1dcd6622f1a9600dced
Leonel Recinos <leonelrecinos1994@gmail.com>
2020-11-12 16:23:57 -0600

    UI
    Se creo la escena del Main Menu y la Seleccion de Karts y se trabajo en la UI

---
+++ Assets/Kart/General/Scripts/MetaController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetaController : MonoBehaviour
+{
+    [Header("Public References")]
+    [SerializeField, SerializeReference]
+    public GameObject LevelControl;
+
+    [Header("Parameters")]
+    public bool isStart;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(isStart){
+            Debug.Log("Meta inicio atravesada");
+            LevelControl.GetComponent<IKartLevel>().StartRace();
+        }else{
+            Debug.Log("MEta fin atravesada");
+            LevelControl.GetComponent<IKartLevel>().FinishRace();
+        }
+    }
+
+}
---
+++ Assets/Kart/Pistas/Santorini Grecia/Kart_SantoriniController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kart_SantoriniController : MonoBehaviour, IKartLevel
+{
+    [Header("Public References")]
+    public CanvasGroup initUI;
+    public CanvasGroup inGameUI;
+    public CanvasGroup finishUI;
+
+    //Variables Privadas
+    enum States{_init, _inGame, _finish};
+    States state;
+    public float raceTime;
+
+    public void StartRace(){
+        state = States._inGame;
+    }
+
+    public void FinishRace(){
+        state = States._finish;
+        SaveRaceResults();
+    }
+
+    public void SaveRaceResults(){
+        Debug.Log("Tu timepo es: " +  Mathf.RoundToInt(raceTime/60) + "min" + Mathf.RoundToInt(raceTime%60) + "seg");
+    }
+
+    void Start(){
+        state = States._init;
+    }
+
+    void Update(){
+        if(state == States._init){
+            if(Input.touchCount > 0){
+                initUI.alpha = 0;
+            }
+        }
+        if(state == States._inGame){
+            raceTime += Time.deltaTime;
+        }
+    }
+}
